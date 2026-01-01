333be8c3e6fc36a93781541e7aaa63a5b29fb5a1
itnproducoes <143436179+itnproducoes@users.noreply.github.com>
2026-01-01 12:06:38 -0300

    Add files via upload

--- lib/ssd1306_bitmap.h
+++ lib/ssd1306_bitmap.h
@@ -6,3 +6,7 @@ extern const uint8_t * ssd16bitlogo_bitmap;
 extern const uint8_t * ssdsegalogo_bitmap;
 extern const uint8_t * ssdextreme_bitmap;
 extern const uint8_t * ssdsoniclogo_bitmap;
+extern const uint8_t * ssdopenheart_bitmap;
+extern const uint8_t * ssdlicense_bitmap;
+
+
